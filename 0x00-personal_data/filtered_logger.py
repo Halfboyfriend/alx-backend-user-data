20f3ebbf74f574d16a4ddfbac1525663571de613
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-08-05 21:58:20 +0100

    filter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,18 @@ Write a function called filter_datum that returns the
 log message obfuscated:
 """
 from typing import List
+import re
 
 
 def filter_datum(fields: List, redaction: str, message: str, separator: str):
-    pass
+    """
+    :param fields:
+    :param redaction:
+    :param message:
+    :param separator:
+    :return:
+    """
+    return re.sub(r'(?<={0})[^{1}]+'
+                  .format(separator,separator)
+                  .join(fields), redaction, message)
+
