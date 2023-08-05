f5fb4d1f7067bf117db2c927d88267556f7aacae
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-08-05 21:53:08 +0100

    taskup

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Write a function called filter_datum that returns the
+log message obfuscated:
+"""
+from typing import List
+
+
+def filter_datum(fields: List, redaction: str, message: str, separator: str):
+    pass
