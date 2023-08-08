5131ba64941c3d7ca15db96705f22b913a504a51
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-08-08 20:01:07 +0100

    task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,3 +5,13 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """basic auth class"""
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Authorizationn with base64"""
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+        
+        if not authorization_header.startswith('Basic '):
+            return None
+        
+        return authorization_header[len('Basic '):]
+        
