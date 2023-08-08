432f92c144354202a3c7c05900464e669a9c3b25
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-08-08 20:02:15 +0100

    task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """A basic authentication frame that inherits from Auth"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -14,4 +15,16 @@ class BasicAuth(Auth):
             return None
         
         return authorization_header[len('Basic '):]
+    
+    
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        if base64_authorization_header is None or not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_value = decoded_bytes.decode('utf-8')
+            return decoded_value
+        except base64.binascii.Error:
+            return None
         
