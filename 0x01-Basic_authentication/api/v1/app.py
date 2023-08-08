b783f67b118dc4d4eef5215eb1fe19a3af99a89d
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-08-08 19:49:17 +0100

    task

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,30 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+auth_type = getenv('AUTH_TYPE', 'auth')
+
+if auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+    
+
+@app.before_request
+def before_request():
+    """Filter user req"""
+    if auth is None:
+        return
+    ex_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if not auth.require_path(request.path, ex_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401, description="Unauthorized")
+    if auth.current_user(request) is None:
+        abort(403, description="Forbidden")
 
 
 @app.errorhandler(404)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A basic authentication frame that inherits from Auth"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """basic auth class"""
