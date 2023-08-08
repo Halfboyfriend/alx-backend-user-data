d0e1fe1987a47220775cb723f82b10253d911718
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-08-08 19:35:10 +0100

    task

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """Auth class"""
+    def require_auth(self, 
+                     path: str, 
+                     excluded_paths: List[str]
+                     ) -> bool:
+        """Returns False, path and excluded path"""
+        return f'{False} - {path} {excluded_paths}'
+    
+
+    def authorization_header(self, request=None) -> str:
+        """Returns None"""
+        return f'{None} - {request}'
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns None"""
+        return f'{None} - {request}'
