d527afbaa58c2425e285281a4e850866202023c9
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-08-09 05:43:23 +0100

    task

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,21 +2,26 @@
 """authentication class"""
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
     """base class for authentication"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """check whether the path is in the excluded path"""
-        if path is None:
-            return True
-        if excluded_paths is None or len(excluded_paths) == 0:
-            return True
-        path = (path + "/") if path[-1] != "/" else path
-        for paths in excluded_paths:
-            if path == paths:
-                return False
+        """Checks if a path requires authentication.
+        """
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
@@ -31,4 +36,3 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """check the snapshot of the user instance"""
         return None
-    
